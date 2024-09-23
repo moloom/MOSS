62ab53815e87449fa64610e62c5c86d0205e1a4b
moloom <moloom777@gmail.com>
2024-09-23 20:38:30 +0800

    init

---
+++ moss/src/main/java/com/moloom/moss/MossApplication.java
@@ -0,0 +1,13 @@
+package com.moloom.moss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MossApplication.class, args);
+    }
+
+}
